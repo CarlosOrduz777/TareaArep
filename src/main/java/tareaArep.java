a0d19896f3e335afd342edfa4f1ac01ba44d2710
unknown <carlos.orduz@labinfo.is.escuelaing.edu.co>
2022-08-17 18:56:15 -0500

    avance TareaArep

---
+++ src/main/java/tareaArep.java
@@ -0,0 +1,23 @@
+
+import static spark.Spark.*;
+public class tareaArep {
+
+    public static void main(String[] args){
+        port(getPort());
+        staticFiles.location("/public");
+        get("/hello",(req, res) -> {
+            String name = req.queryParams("name");
+            return "Hello Heroku" + name;
+        });
+        get("/intraday", (req,res)-> {
+            res.type("application/json");
+            return HttpConnectionExample.getAPIAdvantageIntraDay();
+        });
+    }
+    static int getPort(){
+        if(System.getenv("PORT")!= null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
